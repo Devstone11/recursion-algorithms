ede51d2f908b8015914249b383dc769601001b58
Tyler Bettilyon <teb311@gmail.com>
2016-04-19 16:07:02 -0700

    initial commit:

---
+++ scripts.js
@@ -0,0 +1,34 @@
+
+/**
+ * This function should return the n-th number of the fibonacci sequence. 
+ * This sequence is created by adding the previous two numbers in the sequence
+ * together in order to get the next number. The first 2 are chosen to be 1 
+ * arbitrarily. For example, the first few numbers are:
+ *      1, 1, 2, 3, 5, 8, 13, 21
+ * 
+ * fibonacci(0) === 1
+ * fibonacci(1) === 1
+ * fibonacci(2) === 2
+ * fibonacci(3) === 5
+ * fibonacci(4) === 5
+ ... and so on.
+ */
+function fibonacci(n) {
+
+}
+
+/**
+ * When called this function should print the song "bottles of beer"
+ * for as many bottles as there are 'bottlesRemaining'
+ */
+function bottlesOfBeer(bottlesRemaining) {
+
+}
+
+/** 
+ * This function should return true if the parameter testString is a palindrome
+ * and should return false in all other cases. 
+ */
+function isPalindrome(testString) {
+
+}
